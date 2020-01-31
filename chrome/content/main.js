a4e9ab918db21c9af82232dcdb63a8f7762c5009
John Bieling <john.bieling@gmx.de>
2020-01-31 15:00:36 +0100

    patch for #125

--- chrome/content/main.js
+++ chrome/content/main.js
@@ -1,6 +1,16 @@
 var { gQuicktext } = ChromeUtils.import("chrome://quicktext/content/modules/wzQuicktext.jsm");
 
 var quicktext = {
+  onloadoptions: function ()
+  {
+    window.close();
+  }
+,  
+  onunloadoptions: function ()
+  {
+    this.openSettings();
+  }
+,
   openSettings: function()
   {
     var settingsHandle = window.open("chrome://quicktext/content/settings.xul", "quicktextConfig", "chrome,resizable,centerscreen");
@@ -8,4 +18,8 @@ var quicktext = {
   }
 }
 
-gQuicktext.loadSettings(false);
+// For some reason I cannot add an onload to the overlay "main.xul", so I have
+// to call this manually, but only if it is called from messenger.xul
+if (window.location.href == "chrome://messenger/content/messenger.xul") {
+  gQuicktext.loadSettings(false);
+}
