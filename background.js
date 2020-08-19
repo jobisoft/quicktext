0398806405ec2e1f5d71175c416f669bcd53ca0f
John Bieling <john.bieling@gmx.de>
2020-08-19 11:13:19 +0200

    move background.js into async caller function

--- background.js
+++ background.js
@@ -1,27 +1,29 @@
-messenger.WindowListener.registerDefaultPrefs("defaults/preferences/quicktext.js");
+(async () => { 
+  messenger.WindowListener.registerDefaultPrefs("defaults/preferences/quicktext.js");
 
-messenger.WindowListener.registerChromeUrl([ 
-  ["content",   "quicktext",           "chrome/content/"],
-  ["resource",  "quicktext",           "chrome/"],
-  ["locale",    "quicktext", "de",     "chrome/locale/de/"],
-  ["locale",    "quicktext", "pt-BR",  "chrome/locale/pt_BR/"],
-  ["locale",    "quicktext", "en-US",  "chrome/locale/en-US/"],
-  ["locale",    "quicktext", "es",     "chrome/locale/es/"],
-  ["locale",    "quicktext", "fr",     "chrome/locale/fr/"],
-  ["locale",    "quicktext", "hu",     "chrome/locale/hu/"],
-  ["locale",    "quicktext", "ja",     "chrome/locale/ja/"],
-  ["locale",    "quicktext", "ru",     "chrome/locale/ru/"],
-  ["locale",    "quicktext", "sv-SE",  "chrome/locale/sv-SE/"],
-]);
+  messenger.WindowListener.registerChromeUrl([ 
+    ["content",   "quicktext",           "chrome/content/"],
+    ["resource",  "quicktext",           "chrome/"],
+    ["locale",    "quicktext", "de",     "chrome/locale/de/"],
+    ["locale",    "quicktext", "pt-BR",  "chrome/locale/pt_BR/"],
+    ["locale",    "quicktext", "en-US",  "chrome/locale/en-US/"],
+    ["locale",    "quicktext", "es",     "chrome/locale/es/"],
+    ["locale",    "quicktext", "fr",     "chrome/locale/fr/"],
+    ["locale",    "quicktext", "hu",     "chrome/locale/hu/"],
+    ["locale",    "quicktext", "ja",     "chrome/locale/ja/"],
+    ["locale",    "quicktext", "ru",     "chrome/locale/ru/"],
+    ["locale",    "quicktext", "sv-SE",  "chrome/locale/sv-SE/"],
+  ]);
 
-messenger.WindowListener.registerOptionsPage("chrome://quicktext/content/addonoptions.xhtml")
-  
-messenger.WindowListener.registerWindow(
-  "chrome://messenger/content/messengercompose/messengercompose.xhtml",
-  "chrome://quicktext/content/scripts/messengercompose.js");
+  messenger.WindowListener.registerOptionsPage("chrome://quicktext/content/addonoptions.xhtml")
     
-messenger.WindowListener.registerWindow(
-  "chrome://messenger/content/messenger.xhtml",
-  "chrome://quicktext/content/scripts/messenger.js");
-  
-messenger.WindowListener.startListening();
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/messengercompose/messengercompose.xhtml",
+    "chrome://quicktext/content/scripts/messengercompose.js");
+      
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/messenger.xhtml",
+    "chrome://quicktext/content/scripts/messenger.js");
+    
+  messenger.WindowListener.startListening();
+})();
