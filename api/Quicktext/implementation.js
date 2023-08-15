832f067f5393ec9f6993722fdc10e9ed2e47b8b3
John Bieling <john.bieling@gmx.de>
2023-08-15 08:28:30 +0200

    add basic implementation Quicktext API

--- api/Quicktext/implementation.js
+++ api/Quicktext/implementation.js
@@ -2,8 +2,21 @@
 
 (function (exports) {
 
+  function install(window) {
+    console.log("install");
+  }
+
+  function uninstall(window) {
+    console.log("uninstall");
+  }
+
   class Quicktext extends ExtensionCommon.ExtensionAPI {
     getAPI(context) {
+      function getComposeWindow(windowId) {
+        let { window } = context.extension.windowManager.get(windowId);
+        return window;
+      }
+      
       return {
         Quicktext: {
           openSettings(windowId) {
@@ -15,8 +28,25 @@
               "chrome,resizable,centerscreen"
             );
           },
+          load(windowId) {
+            let window = getComposeWindow(windowId);
+            if (!window || window.document.documentElement.getAttribute("windowtype") != "msgcompose") {
+              return;
+            }
+            install(window);
+          },
+          toggleToolbar(visible) {
+            console.log("toggleToolbar", visible);
+          }
         }
       };
+    }
+
+    onShutdown(isAppShutdown) {
+      if (isAppShutdown) return;
+      for (let window of Services.wm.getEnumerator("msgcompose")) {
+        uninstall(window);
+      }
     }
   };
 
